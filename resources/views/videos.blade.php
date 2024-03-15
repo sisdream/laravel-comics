a6b9695aea850783d1c09f1c3cb66d35a7ab5b43
sisdream <sissipascale2@gmail.com>
2024-03-15 16:00:48 +0100

    feat: create all file

---
+++ resources/views/videos.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="ie=edge" http-equiv="X-UA-Compatible">
+    <title>Document</title>
+
+    @vite('resources/js/app.js')
+  </head>
+
+  <body>
+    @include('partials.header')
+
+    <h1>homepage</h1>
+
+    @include('partials.footer')
+  </body>
+
+</html>

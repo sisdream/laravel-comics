2a2d7e221a0169949dc1a9ffcfa76682254bd4fd
sisdream <sissipascale2@gmail.com>
2024-03-16 16:32:20 +0100

    feat: jumbotron

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,37 +1,43 @@
 <header>
    <section class="dc-power"></section>
    <section class="nav-bar">
-            <ul>
-                <li>
-                    <a @if(Route::currentRouteName() == 'home') class="active" @endif href="{{ route('home')}}">comics</a>
-                </li>
-                <li>
-                    <a  @if(Route::currentRouteName() == 'characters') class="active" @endif href="{{ route('characters')}}">characters</a>
-                </li>
-                <li>
-                    <a @if(Route::currentRouteName() == 'movies') class="active" @endif href="{{ route('movies')}}">movies</a>
-                </li>
-                <li>
-                    <a @if(Route::currentRouteName() == 'tv') class="active" @endif href="{{ route('tv')}}">tv</a>
-                </li>
-                <li>
-                    <a @if(Route::currentRouteName() == 'games') class="active" @endif href="{{ route('games')}}">games</a>
-                </li>
-                <li>
-                    <a @if(Route::currentRouteName() == 'collectibles') class="active" @endif href="{{ route('collectibles')}}">collectibles</a>
-                </li>
-                <li>
-                    <a @if(Route::currentRouteName() == 'videos') class="active" @endif href="{{ route('videos')}}">videos</a>
-                </li>
-                <li>
-                    <a @if(Route::currentRouteName() == 'fans') class="active" @endif href="{{ route('fans')}}">fans</a>
-                </li>
-                <li>
-                    <a @if(Route::currentRouteName() == 'news') class="active" @endif href="{{ route('news')}}">news</a>
-                </li>
-                <li>
-                    <a @if(Route::currentRouteName() == 'shop') class="active" @endif href="{{ route('shop')}}">shop</a>
-                </li>
-            </ul>
-        </section>
+        <div class="logo">
+            <img src="{{ Vite::asset('/resources/images/dc-logo.png')}}" alt="logo">
+        </div>
+        <ul>
+            <li>
+                <a @if(Route::currentRouteName() == 'home') class="active" @endif href="{{ route('home')}}">COMICS</a>
+            </li>
+            <li>
+                <a  @if(Route::currentRouteName() == 'characters') class="active" @endif href="{{ route('characters')}}">CHARACTERS</a>
+            </li>
+            <li>
+                <a @if(Route::currentRouteName() == 'movies') class="active" @endif href="{{ route('movies')}}">MOVIES</a>
+            </li>
+            <li>
+                <a @if(Route::currentRouteName() == 'tv') class="active" @endif href="{{ route('tv')}}">TV</a>
+            </li>
+            <li>
+                <a @if(Route::currentRouteName() == 'games') class="active" @endif href="{{ route('games')}}">GAMES</a>
+            </li>
+            <li>
+                <a @if(Route::currentRouteName() == 'collectibles') class="active" @endif href="{{ route('collectibles')}}">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a @if(Route::currentRouteName() == 'videos') class="active" @endif href="{{ route('videos')}}">VIDEOS</a>
+            </li>
+            <li>
+                <a @if(Route::currentRouteName() == 'fans') class="active" @endif href="{{ route('fans')}}">FANS</a>
+            </li>
+            <li>
+                <a @if(Route::currentRouteName() == 'news') class="active" @endif href="{{ route('news')}}">NEWS</a>
+            </li>
+            <li>
+                <a @if(Route::currentRouteName() == 'shop') class="active" @endif href="{{ route('shop')}}">SHOP</a>
+            </li>
+        </ul>
+    </section>
+    <section class="jumbotron">
+        <img src="{{ Vite::asset('/resources/images/jumbotron.jpg')}}" alt="jumbotron">
+    </section>
 </header>

be4bfdcb778a765c5374509ff9311bc0c3d0b985
sisdream <sissipascale2@gmail.com>
2024-03-18 16:12:57 +0100

    feat: sezione social creata

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -16,74 +16,74 @@
                 <a href="{{ route('tv')}}">TV</a>
             </li>
             <li>
-                <a href="">Games</a>
+                <a href="{{ route('games')}}">Games</a>
             </li>
             <li>
-                <a href="">Videos</a>
+                <a href="{{ route('videos')}}">Videos</a>
             </li>
             <li>
-                <a href="">News</a>
+                <a href="{{ route('news')}}">News</a>
             </li>
             <h3 class="my-2">SHOP</h3>
             <li>
-                <a href="">Shop DC</a>
+                <a href="{{ route('shop')}}">Shop DC</a>
             </li>
             <li>
-                <a href="">Shop DC Collectibles</a>
+                <a href="{{ route('collectibles')}}">Shop DC Collectibles</a>
             </li>
         </ul>
         <ul>
             <h3>DC</h3>
             <li>
-                <a href="">Terms Of Use</a>
+                <a href="#">Terms Of Use</a>
             </li>
             <li>
-                <a href="">Privacy policy (New)</a>
+                <a href="#">Privacy policy (New)</a>
             </li>
             <li>
-                <a href="">Ad Choices</a>
+                <a href="#">Ad Choices</a>
             </li>
             <li>
-                <a href="">Advertising</a>
+                <a href="#">Advertising</a>
             </li>
             <li>
-                <a href="">Jobs</a>
+                <a href="#">Jobs</a>
             </li>
             <li>
-                <a href="">Subscriptions</a>
+                <a href="#">Subscriptions</a>
             </li>
             <li>
-                <a href="">Talent Workshops</a>
+                <a href="#">Talent Workshops</a>
             </li>
             <li>
-                <a href="">CPSC Certificates</a>
+                <a href="#">CPSC Certificates</a>
             </li>
             <li>
-                <a href="">Ratings</a>
+                <a href="#">Ratings</a>
             </li>
             <li>
-                <a href="">Shop Help</a>
+                <a href="{{ route('shop')}}">Shop Help</a>
             </li>
             <li>
-                <a href="">Contact Us</a>
+                <a href="#">Contact Us</a>
             </li>
         </ul>
         <ul>
             <h3>SITES</h3>
             <li>
-                <a href="">DC</a>
+                <a href="#">DC</a>
             </li>
             <li>
-                <a href="">MAD Magazine</a>
+                <a href="#">MAD Magazine</a>
             </li>
             <li>
-                <a href="">DC Kids</a>
+                <a href="#">DC Kids</a>
             </li>
             <li>
-                <a href="">DC Universe</a>
+                <a href="#">DC Universe</a>
             </li>
             <li>
-                <a href="">DC Power Visa</a>
+                <a href="#">DC Power Visa</a>
             </li>
         </ul>
         </div>
@@ -92,6 +92,26 @@
                 <img src="{{ Vite::asset('/resources/images/dc-logo-bg.png')}}" alt="logo">
             </div>
         </div>
-       
+    </section>
+    <section class="social d-flex justify-content-around">
+        <button>SIGN-UP NOW!</button>
+        <div class="d-flex align-items-center gap-3">
+            <h3>FOLLOW US</h3>
+            <div>
+                <img src="{{ Vite::asset('/resources/images/footer-facebook.png')}}" alt="facebook">
+            </div>
+            <div>
+                <img src="{{ Vite::asset('/resources/images/footer-twitter.png')}}" alt="twitter">
+            </div>
+            <div>
+                <img src="{{ Vite::asset('/resources/images/footer-youtube.png')}}" alt="youtube">
+            </div>
+            <div>
+                <img src="{{ Vite::asset('/resources/images/footer-pinterest.png')}}" alt="pinterest">
+            </div>
+            <div>
+                <img src="{{ Vite::asset('/resources/images/footer-periscope.png')}}" alt="periscope">
+            </div>
+        </div>
     </section>
 </footer>
